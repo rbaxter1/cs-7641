87644e51a6d4e0efe789c0380018a80f865606ec
Rob Baxter <Rob Baxter>
2016-11-06 02:17:15 -0500

    updated

---
+++ Assignment3/run.py
@@ -0,0 +1,17 @@
+from part1 import *
+from part2 import *
+from part3 import *
+
+def main():
+    p1 = part1()
+    p1.main()
+    
+    p2 = part2()
+    p2.main()
+    
+    p3 = part3()
+    p3.main()
+
+if __name__== '__main__':
+    main()
+    
